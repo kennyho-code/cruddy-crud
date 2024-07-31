552954ad13b83330004da81292a3d6a7c787e089
Kenny Ho <kennyho.code@gmail.com>
2024-07-31 13:10:09 -0400

    remove supabase

--- src/components/layout/Header/Profile.tsx
+++ src/components/layout/Header/Profile.tsx
@@ -3,26 +3,15 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { createClient } from "@/utils/supabase/server";
-import Link from "next/link";
-
-async function getUserId() {
-  const supabase = createClient();
-  const data = (await supabase.auth.getUser()).data;
-  return data.user?.id;
-}
 
 async function Profile() {
-  const userId = await getUserId();
-  return userId ? (
+  return (
     <>
       <Popover>
         <PopoverTrigger>👤</PopoverTrigger>
         <PopoverContent>Place content for the popover here.</PopoverContent>
       </Popover>
     </>
-  ) : (
-    <Link href="/login">Login</Link>
   );
 }
 
